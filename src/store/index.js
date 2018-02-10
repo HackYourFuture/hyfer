0a7473ed0d837efc161fdc07378ddf0e068bf7ca
JiDarwish <ji.darwish98@gmail.com>
2018-02-10 21:37:53 +0100

    Added uiStore to keep track of user auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import TimelineStore from './TimeLineStore';
 import ModuleInfoStore from './ModuleInfoStore';
+import UiStore from './UiStore';
 
 export const timelineStore = (window.timelineStore = new TimelineStore());
 export const moduleInfoStore = (window.moduleInfoStore = new ModuleInfoStore());
+export const uiStore = (window.uiStore = new UiStore());
