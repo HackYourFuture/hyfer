eb4606bee437e8fd3c8063f555d4acc02c4a9c9c
itemah26 <ite.mah.26@gmail.com>
2018-02-20 14:48:32 +0100

    Module add is done

--- src/Pages/Modules/ModuleFooter.js
+++ src/Pages/Modules/ModuleFooter.js
@@ -3,12 +3,14 @@ import style from  '../../assets/styles/modules.css';
 import ModuleButton from './ModuleButton';
 import ModuleObservable from './ModuleObservable';
 import ModuleServiceBack from './ModuleServiceBack';
+import ModuleForm from './ModuleForm';
+//import { triggerAsyncId } from 'async_hooks';
 
 export default class ModuleFooter extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {isChanged:false};
+    this.state = {isChanged:false, isadding: false};
   }
 
   componentDidMount = () => {
@@ -27,18 +29,33 @@ export default class ModuleFooter extends Component {
     })
   }
 
-  AddModule = (e) =>{
-    alert('Add')
+  AddModule = (module) =>{
+    const curModulesArr = ModuleObservable.getModules();
+    const newModulesArr = Array.from(curModulesArr);
+    newModulesArr.push(module);
+    ModuleObservable.setModules(newModulesArr);
+    this.SaveChanges();
+    this.togglePopup();
   }
 
+  togglePopup = () => {
+    this.setState({
+      isadding: !this.state.isadding
+    });
+  }
+  
   render() {
     return (
-        <div className={style.moduleFooter}>
+      <div className={style.moduleFooter}>
             <ModuleButton action="undo" title="Undo Changes" disabled={!this.state.isChanged} clickHandler={this.UndoChanges} />
             <span className={style.moduleButtonSep}></span>
             <ModuleButton action="save" title="Save Changes" disabled={!this.state.isChanged} clickHandler={this.SaveChanges} />
-            <ModuleButton action="add" title="Add module" disabled={false} clickHandler={this.AddModule} />
-        </div>
+            <ModuleButton action="add" title="Add module" disabled={false} clickHandler={this.togglePopup} />
+            {this.state.isadding ?
+            <ModuleForm text='Adding module...' Cansel={this.togglePopup} Add={this.AddModule}/>
+            :null
+            }
+      </div>
         
     );
   }
