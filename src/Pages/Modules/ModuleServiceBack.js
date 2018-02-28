af64a693a4dc22a876848353724065ebf1a8ef4c
meyadd <eyad.aldarra@gmail.com>
2018-02-28 23:14:39 +0100

    auth update

--- src/Pages/Modules/ModuleServiceBack.js
+++ src/Pages/Modules/ModuleServiceBack.js
@@ -1,8 +1,14 @@
 const api_url = "http://localhost:3000/api"
 
+
 class moduleServiceBack {
     loadModules(callBack){
-		fetch(`${api_url}/modules`)
+        const token = localStorage.getItem("token")
+		fetch(`${api_url}/modules` , {
+            credentials: "same-origin",
+            headers: {
+            'Authorization':'Bearer ' + token,
+        }})
 		.then(res => res.json())
 		.then(resJson => {
             callBack(resJson)
@@ -14,10 +20,13 @@ class moduleServiceBack {
     }
 
     saveModules(modules , callBack) {
+        const token = localStorage.getItem("token")
         fetch(`${api_url}/modules`, {
               method: 'PATCH',
+              credentials: "same-origin",
               headers: {
                   'Content-Type': 'application/json',
+                  'Authorization':'Bearer ' + token,
               },
               body: JSON.stringify(modules),
           })
