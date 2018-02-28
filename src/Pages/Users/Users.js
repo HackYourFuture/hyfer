8b292ff1d0a3ca65d2544bcfa9bf7da890dcabe2
pesh12 <ekocibar@hotmail.com>
2018-02-28 11:50:53 +0100

    some minor styles

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -3,6 +3,15 @@ import store from '../../store/UserStore';
 import styles from '../../assets/styles/users.css';
 import { Link } from 'react-router-dom';
 
+import FaGraduationCap from 'react-icons/lib/fa/graduation-cap';
+import FaClockO from 'react-icons/lib/fa/clock-o';
+import MdEmail from 'react-icons/lib/md/email';
+import FaSlack from 'react-icons/lib/fa/slack';
+import FaFire from 'react-icons/lib/fa/fire';
+import FaGithub from 'react-icons/lib/fa/github';
+import MdClass from 'react-icons/lib/md/class';
+
+
 export default class Users extends React.Component {
 
   componentWillMount = () => {
@@ -72,14 +81,14 @@ export default class Users extends React.Component {
                        src={`https://avatars.githubusercontent.com/${user.username}`} 
                        alt={`Profile - ${user.username}`}
                        onError={(e)=>{e.target.src=`https://api.adorable.io/avatars/100/${user.full_name}`} }/>
-                  <div className ={user.userName}>{user.full_name}</div>
-                  <div>{user.group_name}</div>
-                  <div>{user.role}</div>
-                  <div>{user.register_date}</div>
-                  <div>{user.email}</div>
-                  <div>{user.slack_username}</div>
-                  <div>{user.freecodecamp_username}</div>
-                  <div>{user.username}</div>
+                  <div className ={styles.userName}>{user.full_name}</div>
+                  <div><MdClass />{user.group_name}</div>
+                  <div><FaGraduationCap/> {user.role}  </div>
+                  <div><FaClockO />{user.register_date}</div>
+                  <div><MdEmail />{user.email}</div>
+                  <div><FaSlack />{user.slack_username}</div>
+                  <div><FaFire />{user.freecodecamp_username}</div>
+                  <div><FaGithub />{user.username}</div>
                 </div>
                 <form className = {styles.editUser} onClick={()=>{ 
                   console.log("FROM EDIT BUTTON",user)
@@ -89,11 +98,11 @@ export default class Users extends React.Component {
                     full_name: user.full_name,
                     group_name: user.group_name,
                     role: user.role,
+                    register_date:user.register_date,
                     email: user.email,
                     slack_username: user.slack_username,
                     freecodecamp_username: user.freecodecamp_username,
                     mobile: user.mobile,
-                    register_date:user.register_date,
                     group_id:user.group_id
                   });
                 
