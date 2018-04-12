293cf2a6e55184fb2ec24040c5d1f43a336ca5ac
chileluk <32581924+chileluk@users.noreply.github.com>
2018-04-12 15:57:59 -0400

    fix indentation & other formatting issues in Modules folder

--- src/Pages/Modules/ModuleButton.js
+++ src/Pages/Modules/ModuleButton.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import style from  '../../assets/styles/modules.css';
+import React, { Component } from 'react'
+import style from  '../../assets/styles/modules.css'
+
 
 export default class ModuleButton extends Component {
 
     /*buttonClicked = ()=>{
-      alert('click');
+      alert('click')
     }*/
 
-  render() {
-    return (
-        <button type="button" className={style.moduleButton} 
-        title={this.props.title} 
-        disabled={this.props.disabled} 
-        onClick={this.props.clickHandler}>
-          <md-icon class="material-icons">{this.props.action}</md-icon>
-        </button>
-        
-    );
-  }
+    render() {
+        return (
+            <button type="button"
+                className={style.moduleButton} 
+                title={this.props.title} 
+                disabled={this.props.disabled} 
+                onClick={this.props.clickHandler}>
+                <md-icon class="material-icons">{this.props.action}</md-icon>
+            </button>
+        )
+    }
 }
--- src/Pages/Modules/ModuleFooter.js
+++ src/Pages/Modules/ModuleFooter.js
@@ -1,69 +1,87 @@
-import React, { Component } from 'react';
-import style from  '../../assets/styles/modules.css';
-import ModuleButton from './ModuleButton';
-import ModuleObservable from './ModuleObservable';
-import ModuleServiceBack from './ModuleServiceBack';
-import ModuleForm from './ModuleForm';
-import Notifications, {notify} from 'react-notify-toast';
-//import { triggerAsyncId } from 'async_hooks';
+import React, { Component } from 'react'
+import style from  '../../assets/styles/modules.css'
+import ModuleButton from './ModuleButton'
+import ModuleObservable from './ModuleObservable'
+import ModuleServiceBack from './ModuleServiceBack'
+import ModuleForm from './ModuleForm'
+import Notifications, {notify} from 'react-notify-toast'
+//import { triggerAsyncId } from 'async_hooks'
 
 export default class ModuleFooter extends Component {
 
-  state = {
-    isChanged: false,
-    isadding: false
-  };
+    state = {
+        isChanged: false,
+        isadding: false
+    }
 
-  componentDidMount = () => {
-    ModuleObservable.subscribe((newArr) => {
-				this.setState({isChanged: ModuleObservable.isChanged()})
-    })
-  };
+    componentDidMount = () => {
+        ModuleObservable.subscribe((newArr) => {
+            this.setState({ isChanged: ModuleObservable.isChanged() })
+        })
+    }
 
-  UndoChanges = () => {
-    ModuleObservable.resetModules()
-    notify.show('Your changes have been cancelled !', 'warning');
-  }
+    UndoChanges = () => {
+        ModuleObservable.resetModules()
+        notify.show('Your changes have been cancelled !', 'warning')
+    }
 
-  SaveChanges = () => {
-    ModuleServiceBack.saveModules(ModuleObservable.getModules() , ()=> {
-      ModuleServiceBack.loadModules((result)=> ModuleObservable.initModules(result))
-      notify.show('Your changes are successfully Saved !', 'success')
-    })
-  }
+    SaveChanges = () => {
+        ModuleServiceBack.saveModules(ModuleObservable.getModules(), () => {
+            ModuleServiceBack.loadModules((result) => ModuleObservable.initModules(result))
+            notify.show('Your changes are successfully Saved !', 'success')
+        })
+    }
 
-  AddModule = (module) =>{
-    const curModulesArr = ModuleObservable.getModules();
-    const newModulesArr = Array.from(curModulesArr);
-    
-    let maxId = curModulesArr.map( m=> m.id ).reduce(( max, cur ) => Math.max( max, cur ))
-    module.id = maxId + 1
-    newModulesArr.push(module);
+    AddModule = (module) => {
+        const curModulesArr = ModuleObservable.getModules()
+        const newModulesArr = Array.from(curModulesArr)
+        
+        let maxId = curModulesArr.map( m => m.id ).reduce(( max, cur ) => Math.max(max, cur))
+        module.id = maxId + 1
+        newModulesArr.push(module)
 
-    ModuleObservable.setModules(newModulesArr);
-  }
+        ModuleObservable.setModules(newModulesArr)
+    }
 
-  showAddModal = () => {
-    this.setState({
-      isadding: true
-    });
-  }
-  hideAddModal = () => {
-    this.setState({
-      isadding: false
-    });
-  }
+    showAddModal = () => {
+        this.setState({
+        isadding: true
+        })
+    }
+        
+    hideAddModal = () => {
+        this.setState({
+        isadding: false
+        })
+    }
   
-  render() {
-    return (
-      <div className={style.moduleFooter}>
-            <Notifications />
-            <ModuleButton action="undo" title="Undo Changes" disabled={!this.state.isChanged} clickHandler={this.UndoChanges} />
-            <span className={style.moduleButtonSep}></span>
-            <ModuleButton action="save" title="Save Changes" disabled={!this.state.isChanged} clickHandler={this.SaveChanges} />
-            <ModuleButton action="add" title="Add module" disabled={false} clickHandler={this.showAddModal} />
-            <ModuleForm onCancel={this.hideAddModal} onAdd={this.AddModule} visible={this.state.isadding} title="Adding Module.." actionName="ADD" />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={style.moduleFooter}>
+                <Notifications />
+                <ModuleButton action="undo"
+                    title="Undo Changes"
+                    disabled={!this.state.isChanged}
+                    clickHandler={this.UndoChanges}
+                />
+                <span className={style.moduleButtonSep}></span>
+                <ModuleButton action="save"
+                    title="Save Changes"
+                    disabled={!this.state.isChanged}
+                    clickHandler={this.SaveChanges}
+                />
+                <ModuleButton action="add"
+                    title="Add module"
+                    disabled={false}
+                    clickHandler={this.showAddModal}
+                />
+                <ModuleForm onCancel={this.hideAddModal}
+                    onAdd={this.AddModule}
+                    visible={this.state.isadding}
+                    title="Adding Module.."
+                    actionName="ADD"
+                />
+            </div>
+        )
+    }
 }
