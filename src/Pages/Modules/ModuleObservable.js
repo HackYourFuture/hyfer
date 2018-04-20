1b07ddc4a8c8beb278f3c515052b28234fb8effd
chileluk <32581924+chileluk@users.noreply.github.com>
2018-04-20 10:01:00 -0400

    restore changes overwritten by last merge

--- src/Pages/Modules/ModuleObservable.js
+++ src/Pages/Modules/ModuleObservable.js
@@ -1,40 +1,40 @@
 class Observable {
     constructor() {
-        this._observers = [];
-        this._modules = [];
-        this._isChanged = false;
-        this._serverModules = [];
-        
+        this._observers = []
+        this._modules = []
+        this._isChanged = false
+        this._serverModules = []
+
         this.subscribe = (f) => {
-            this._observers.push(f);
-        };
+            this._observers.push(f)
+        }
         this.unsubscribe = (f) => {
-            this._observers = this._observers.filter(e => e !== f);
-        };
+            this._observers = this._observers.filter(e => e !== f)
+        }
         this.getModules = () => {
-            return this._modules;
-        };
-        this.setModules = (new_modules) => {
-            this._isChanged = true;
-            this._modules = new_modules;
+            return this._modules
+        }
+        this.setModules = (new_moules) => {
+            this._isChanged = true
+            this._modules = new_moules
             for (let f of this._observers)
-                f(new_modules);
-        };
-        this.initModules = (server_modules) => {
-            this._serverModules = server_modules;
-            this._isChanged = false;
-            this._modules = server_modules.map(a => ({ ...a }));
+                f(new_moules)
+        }
+        this.initModules = (server_moules) => {
+            this._serverModules = server_moules
+            this._isChanged = false
+            this._modules = server_moules.map(a => ({ ...a }))
             for (let f of this._observers)
-                f(server_modules);
-        };
+                f(server_moules)
+        }
         this.isChanged = () => {
-            return this._isChanged;
-        };
+            return this._isChanged
+        }
         this.resetModules = () => {
-            this.initModules(this._serverModules);
-        };
-    }
+            this.initModules(this._serverModules)
+        }
+    }    
 }
 
 const ModuleObservable = new Observable()
-export default ModuleObservable;
+export default ModuleObservable
