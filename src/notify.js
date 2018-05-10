e95091b7936b789b58de5cb48584418393b49f5d
JawharBairakdar <j.birakdar@gmail.com>
2018-05-10 03:56:16 +0200

    developing (error_bundle)

--- src/notify.js
+++ src/notify.js
@@ -2,10 +2,14 @@ import { notify } from 'react-notify-toast'
 
 export const error_bundle = (e) => {
     // no stack creepy Errors
-    const isString = e && typeof e === 'string' && e.length < 100
+    const isString = e && typeof e === 'string' && e.length < 100 && !e.stack
     const isNumber = typeof e === 'number'
-    
-    if (e.message) return notify.show(e.message, 'error')
-    if (isString || isNumber) return notify.show(e, 'error')
-    else return notify.show('Oops Something Went Wrong!', 'error')
+    const error = param => notify.show(param, 'error')
+
+    if (e.message)
+        return error(e.message)
+    if (isString || isNumber)
+        return error(e)
+    else
+        return error('Oops Something Went Wrong!')
 }
