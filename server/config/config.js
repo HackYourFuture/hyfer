c68dc186edcfe08502437fd44ecf79bd1f050510
malnajar <m.n.friendi@gmail.com>
2018-05-10 13:12:46 +0200

    adding a new feature synchronizing the data from github API

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: 'localhost',
+  user: 'm.najar',
+  password: '00120012',
+  database: 'hyfer2',
+  clientID: 'b607eccc1941bd82be54',
+  clientSecret: '8bbc98379e951f44ff20a5242abd9353dde17e03',
+  callbackURL: 'http://localhost:3005/auth/github/callback',
+  url: 'http://localhost:3000',
+  jwtSecret: 'hyferSecret',
+  githubTeamKey: 'eb62095e2144e7d745f52d6cba55c123e332d7d0' 
+  
+}
+
