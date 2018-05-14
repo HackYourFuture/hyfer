201f862c8049f765636613e8fba87d59aca7e6d5
yousefallaban <yousefallaban@gmail.com>
2018-05-14 20:39:46 +0200

    add steps

---
+++ src/Pages/TrainTicket/Wrap.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class wrap extends Component {
+
+  render() {
+
+    return (
+      <div>
+        wrap
+      </div>
+    );
+  }
+}
+
+export default wrap
