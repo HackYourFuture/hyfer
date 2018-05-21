6186262037262cab3041a16dc53c48fb03a84ef9
JawharBairakdar <j.birakdar@gmail.com>
2018-05-21 17:17:58 +0200

    correcting catching errors by propagation

--- src/Pages/Modules/ModuleServiceBack.js
+++ src/Pages/Modules/ModuleServiceBack.js
@@ -1,38 +1,34 @@
-import { error_bundle } from '../../notify'
 
 const api_url = 'http://localhost:3000/api'
 
 class moduleServiceBack {
-    
+
     async loadModules(callBack) {
         const token = localStorage.getItem('token')
-        try {
-            const res = await fetch(`${api_url}/modules`, {
-                credentials: 'same-origin',
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            })
-            if (!res.ok) throw res
-            const resJson = await res.json()
-            callBack(resJson)
-        } catch (err) { error_bundle(err) }
+        const res = await fetch(`${api_url}/modules`, {
+            credentials: 'same-origin',
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        })
+        if (!res.ok) throw res
+        const resJson = await res.json()
+        callBack(resJson)
     }
 
     async saveModules(modules, callBack) {
         const token = localStorage.getItem('token')
-        try {
-            await fetch(`${api_url}/modules`, {
-                method: 'PATCH',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: 'Bearer ' + token
-                },
-                body: JSON.stringify(modules)
-            })
-            await callBack()
-        } catch (err) { error_bundle(err) }
+        const res = await fetch(`${api_url}/modules`, {
+            method: 'PATCH',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            body: JSON.stringify(modules)
+        })
+        if (!res.ok) throw res
+        await callBack()
     }
 }
 
