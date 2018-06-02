da90643d7076c0c9765081b2d187e79e7dd58612
Jim Cramer <remarcmij@gmail.com>
2018-06-02 21:45:17 +0200

    add error log function

--- hyfer-backend/server/util/logger.js
+++ hyfer-backend/server/util/logger.js
@@ -5,5 +5,6 @@ winston.level = process.env.LOG_LEVEL || 'debug';
 module.exports = {
   info: (...args) => winston.info(...args),
   log: (...args) => winston.log(...args),
+  error: (...args) => winston.error(...args),
   debug: (...args) => winston.debug(...args),
 };
