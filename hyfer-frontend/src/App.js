13ff7ef91c322525cd123bad0a8969b0f8c1e491
Jim Cramer <remarcmij@gmail.com>
2018-07-14 02:40:20 +0200

    Remove unused routes/pages

--- hyfer-frontend/src/App.js
+++ hyfer-frontend/src/App.js
@@ -12,22 +12,16 @@ import Homework from './Pages/Homework/Homework';
 import Modules from './Pages/Modules/Modules';
 import TimeLine from './Pages/Timeline/TimeLine';
 import TrainTicket from './Pages/TrainTicket/TrainTicket';
-import CurrentUserProfile from './Pages/Users/CurrentUserProfile';
-import Profile from './Pages/Users/Profile';
-import UserAccount from './Pages/Users/UserAccount';
 import Users from './Pages/Users/Users';
 
 const PUBLIC_ROUTES = [
   { exact: true, path: '/timeline', component: TimeLine },
-  { exact: true, path: '/currentUserProfile', component: CurrentUserProfile },
 ];
 
 const ROUTES = {
   teacher: [
     { exact: true, path: '/modules', component: Modules },
     { exact: true, path: '/users', component: Users },
-    { exact: true, path: '/profile', component: Profile },
-    { exact: true, path: '/userAccount', component: UserAccount },
     { exact: true, path: '/homework', component: Homework },
     { exact: true, path: '/homework/:classNumber', component: Homework },
     { exact: true, path: '/TrainTicket', component: TrainTicket },
@@ -58,9 +52,6 @@ class App extends Component {
     token = window.localStorage.getItem('token');
     if (token) {
       await this.props.global.fetchCurrentUser();
-      // TODO: remove this once we are sure we have removed the last residues of
-      // references to currentUser from the old store
-      // await userStore.loadUser();
     } else {
       window.localStorage.removeItem('token');
       this.setState({ profile: defaultProfile });
