47c1b1974a5acbf8133cddd4359a47b23f16fc7a
Jim Cramer <remarcmij@gmail.com>
2018-07-14 02:39:13 +0200

    Remove unused code and simplify queries

--- hyfer-backend/server/datalayer/users.js
+++ hyfer-backend/server/datalayer/users.js
@@ -1,9 +1,4 @@
-const {
-  execQuery,
-  beginTransaction,
-  commit,
-  rollback,
-} = require('./database');
+const { execQuery } = require('./database');
 
 const GET_USERS = `
   SELECT users.*, \`groups\`.group_name 
@@ -11,9 +6,8 @@ const GET_USERS = `
   LEFT JOIN group_students ON users.id=group_students.user_id      
   LEFT JOIN \`groups\` ON \`groups\`.id=group_students.group_id`;
 
-const UPDATE_USER = `
-  UPDATE users SET full_name=?, role=?, email=?, mobile=?
-  WHERE id=?`;
+const UPDATE_USER =
+  'UPDATE users SET email=?, linkedin_username=? WHERE id=?';
 
 const USERS_MODULE_STUDENTS = `
   SELECT users.id , users.full_name, users.role, users.username,
@@ -34,16 +28,12 @@ function getUsers(con) {
   return execQuery(con, `${GET_USERS} ORDER BY full_name`);
 }
 
-function getUserProfile(con, username) {
-  return execQuery(con, 'SELECT username, full_name, role FROM users WHERE username=?', username);
-}
-
 function getUserByUsername(con, username) {
-  return execQuery(con, `${GET_USERS} WHERE username=?`, username);
+  return execQuery(con, 'SELECT * FROM users WHERE username=?', username);
 }
 
 function getUserById(con, id) {
-  return execQuery(con, `${GET_USERS} WHERE users.id=?`, id);
+  return execQuery(con, 'SELECT * FROM users WHERE users.id=?', id);
 }
 
 function getUsersByGroup(con, groupId) {
@@ -61,8 +51,8 @@ function getTeachersByRunningModule(con, runningId) {
 async function addUser(con, user) {
   const { insertId } = await execQuery(
     con,
-    'INSERT INTO users (username, access_token, full_name, email, role) VALUES(?,?,?,?,?)',
-    [user.username, user.access_token, user.full_name, user.email, user.role]
+    'INSERT INTO users (username, full_name, email, role) VALUES(?,?,?,?)',
+    [user.username, user.full_name, user.email, user.role]
   );
   return insertId;
 }
@@ -90,35 +80,13 @@ async function bulkUpdateMemberships(con, groupAndUserIds) {
   return execQuery(con, 'INSERT INTO group_students (group_id, user_id) VALUES ?', [groupAndUserIds]);
 }
 
-async function updateUser(con, id, user) {
-  try {
-    await beginTransaction(con);
-    await execQuery(con, UPDATE_USER, [
-      user.full_name,
-      user.role,
-      user.linkedin_username,
-      user.email,
-      user.mobile,
-      id,
-    ]);
-    await execQuery(con, 'DELETE FROM group_students WHERE user_id=?', [id]);
-    if (user.group_id) {
-      await execQuery(
-        con,
-        'INSERT INTO group_students (user_id, group_id) VALUES(?,?)',
-        [id, user.group_id]
-      );
-    }
-    await commit(con);
-  } catch (err) {
-    await rollback(con);
-    throw err;
-  }
+function updateUser(con, id, data) {
+  const { email, linkedInName } = data;
+  return execQuery(con, UPDATE_USER, [email, linkedInName, id]);
 }
 
 module.exports = {
   getUsers,
-  getUserProfile,
   getUserByUsername,
   getUsersByGroup,
   getTeachersByRunningModule,
